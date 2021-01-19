e60196da9e604479e9ab000665ca3b787fa52e32
yaredambaye <ytsehaye@umd.edu>
2021-01-19 13:10:43 -0500

    api post form call completed

---
+++ src/Components/ImageDrop/ImageDrop.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { render } from "react-dom";
+import { Formik } from "formik";
+
+import Dropzone from "react-dropzone";
+
+const dropzoneStyle = {
+  width: "100%",
+  height: "auto",
+  borderWidth: 2,
+  borderColor: "rgb(102, 102, 102)",
+  borderStyle: "dashed",
+  borderRadius: 5,
+};
+
+const ImageDrop = () => {
+  return <div></div>;
+};
+
+export { ImageDrop };
